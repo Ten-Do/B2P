1bf7cf9bd0ec6f07c967aabd01512f87234aca7b
otter00 <ivlevanastia2001@mail.ru>
2023-07-02 13:04:39 +0300

    styles + svg positioning

--- form-templates/src/components/Unipay/Unipay.jsx
+++ form-templates/src/components/Unipay/Unipay.jsx
@@ -9,7 +9,7 @@ import UnipayStyles from './Unipay.module.scss';
 
 /* ICONS */
 // svg inline 
-import AmountValue from '../../assets/unipay-input-value.png';
+import AmountValue from '../../assets/unipay-input-value.svg';
 
 export default function Unipay() {
 
@@ -37,23 +37,28 @@ export default function Unipay() {
         
         <h1 className={UnipayStyles.form__title}>Создание заказа</h1>
 
-        <label>Введите сумму заказа</label>
+        <div className={UnipayStyles.input__container}>
+        <label>Введите сумму заказа
         <figure className={UnipayStyles.amount__icon}>
           <img src={AmountValue} alt="amount__value" />
         </figure>
         <input className={UnipayStyles.amount__input} type='text' {...register("amount", {required: true, minLength:1, maxLength: 7})} placeholder="0" />
         {errors.amount && <p>Поле обязательно</p>}
+        </label>
 
-        <label>Email</label>
+        <label>Email
         <input type='email' {...register("email", {required: true})} placeholder="example@mail.com" />
         {errors.email && <p>Поле обязательно</p>}
+        </label>
 
-        <label>Описание</label>
+        <label>Описание
         <input type='text'
           {...register("description", { required: true})} placeholder="что-то о заказе"/>
         {errors.description && <p>Поле обязательно</p>}
+        </label>
+        </div>
 
-        <button className={UnipayStyles.submit__button} type="button">Создать</button>
+        <button className={UnipayStyles.submit__button} type="submit">Создать</button>
       </form>
     );
 }
