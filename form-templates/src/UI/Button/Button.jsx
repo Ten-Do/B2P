309a548869a3c2238984c05af3b9586765636532
Ten-Do <107281193+Ten-Do@users.noreply.github.com>
2023-07-02 17:18:29 +0300

    Update Button.jsx

--- form-templates/src/UI/Button/Button.jsx
+++ form-templates/src/UI/Button/Button.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Button(props) {
       return <button 
       className={props.className}
-      type={props}
-      >{props.name}</button>;
+      type={props.type}
+      >{props.children}</button>;
   }
 
 export default Button;
